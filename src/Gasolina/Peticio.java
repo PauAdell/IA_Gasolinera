39a3364abce28971f470322c78665c54c7f61d20
Jaya Gracias <jayonga3@gmail.com>
2021-10-20 14:41:24 +0200

    Peticio amb atribut cisternaAssignada

--- src/Gasolina/Peticio.java
+++ src/Gasolina/Peticio.java
@@ -2,9 +2,11 @@ package Gasolina;
 
 public class Peticio {
     private int dia;
+    private int cisternaAssignada;
     private Posicio posGaso;
 
     public Peticio(int cx, int cy, int d) {
+        cisternaAssignada = -1;
         dia = d;
         posGaso = new Posicio( cx, cy);
     }
@@ -13,16 +15,21 @@ public class Peticio {
         return this.dia;
     }
 
-    public void setDia(int newDia) {
-        dia = newDia;
-    }
-
     public Posicio getPos() {
         return this.posGaso;
+    }
+
+    public int getCisternaAssignada() { return this.cisternaAssignada; }
+
+    public void setDia(int newDia) {
+        dia = newDia;
     }
 
     public void setPos( Posicio p) {
         posGaso = p;
     }
+
+    public void setCisternaAssignada(int ca) { cisternaAssignada = ca; }
+
 
 }
